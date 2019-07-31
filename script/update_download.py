d8f8adba6883f497e105ebddafd1fa9938408ad8
laubonghaudoi <laubonghaudoi@icloud.com>
2019-07-31 10:34:17 -0700

    Update download

--- script/update_download.py
+++ script/update_download.py
@@ -5,8 +5,15 @@
 將`sources`文件夾中的文件複製到對應的`download`文件夾。
 配合`update_submodules.sh`可更新`download`文件夾中的碼表。
 """
-import yaml, shutil
-info = yaml.load(open("source_info.yaml", "r", encoding="utf-8"), Loader=yaml.SafeLoader)
+
+import shutil
+
+import yaml
+
+info = yaml.load(open("source_info.yaml", "r",
+                      encoding="utf-8"), Loader=yaml.SafeLoader)
+
 for dowload in info.keys():
     for file in info[dowload]["files"]:
-        shutil.copy("../sources/"+info[dowload]["source"]+file, "../download/"+dowload)
+        shutil.copy("../sources/"+info[dowload]
+                    ["source"]+file, "../download/"+dowload)
