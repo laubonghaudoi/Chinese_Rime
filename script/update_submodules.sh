b93731972c2d11cb72f0cecc74f16a157bf5b4ac
shinzoqchiuq <shinzoqchiuq@outlook.com>
2021-12-05 23:11:49 +0800

    Update 晋语汾阳话输入方案

--- script/update_submodules.sh
+++ script/update_submodules.sh
@@ -10,6 +10,11 @@ do
         git fetch --all
         git reset --hard origin/棗莊話羅馬字輸入方案
         git pull origin 棗莊話羅馬字輸入方案
+    elif [ "$file" = "./晉語/晋语汾阳话输入方案" ];
+    then
+        git fetch --all
+        git reset --hard origin/main
+        git pull origin main
     else
         git fetch --all
         git reset --hard origin/master
